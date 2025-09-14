ed037c2c72f099cdac85b094427473bc9f4e0f7e
khamzaevasad <135143741+khamzaevasad@users.noreply.github.com>
2025-09-15 04:21:40 +0900

    fix: update filter option from "America" to "Americas" for consistency

--- src/js/action.js
+++ src/js/action.js
@@ -17,8 +17,18 @@ input.addEventListener("input", () => {
 document.addEventListener("DOMContentLoaded", () => {
   const select = document.querySelector(".select__filter");
 
-  select.addEventListener("change", () => {
-    const selectVal = this.value;
-    console.log(selectVal);
+  select.addEventListener("change", (e) => {
+    const region = document.querySelectorAll(".country-region");
+    const card = document.querySelectorAll(".country-card ");
+
+    const optionVal = e.target.value;
+
+    region.forEach((item, i) => {
+      if (item.textContent == optionVal || optionVal == "All") {
+        card[i].style.display = "flex";
+      } else {
+        card[i].style.display = "none";
+      }
+    });
   });
 });
